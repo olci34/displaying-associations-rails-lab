e4f7f55cc9c1980e1f57952888d7413a0f9fb2d9
github username <github email address>
2021-03-17 13:26:48 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bootsnap'
 
 group :development, :test do
   gem 'pry'
+  gem 'require_all'
   gem 'capybara'
   gem 'rspec-rails'
 end
--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -1,8 +1,8 @@
-class CreateSongs < ActiveRecord::Migration
+class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :title
-
+      t.integer :artist_id
       t.timestamps null: false
     end
   end
